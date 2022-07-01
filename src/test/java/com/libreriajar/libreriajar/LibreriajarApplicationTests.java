1aa5edc0eb427d74d98dec26bbdf31c19bcde750
Reiko890 <eric.orlando.pc@gmail.com>
2022-06-30 19:10:39 -0500

    Add files via upload

---
+++ src/test/java/com/libreriajar/libreriajar/LibreriajarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libreriajar.libreriajar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriajarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
